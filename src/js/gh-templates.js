89bff0300ad1a428dbc73eefced073c0621e1759
Pablissimo <turnacre@hotmail.com>
2014-02-02 17:21:57 +0000

    Final few template types that I intend to tackle

--- src/js/gh-templates.js
+++ src/js/gh-templates.js
@@ -89,16 +89,14 @@ function program6(depth0,data) {
 Handlebars.partials['gh-DeleteEvent'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, functionType="function", self=this;
+  var buffer = "", stack1, helper, options, functionType="function", escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing, self=this;
 
 function program1(depth0,data) {
   
   var buffer = "", stack1, helper, options;
-  buffer += "\r\n<span class=\"action\">Deleted branch <a href=\""
-    + escapeExpression((helper = helpers.branchUrl || (depth0 && depth0.branchUrl),options={hash:{},data:data},helper ? helper.call(depth0, ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.ref), options) : helperMissing.call(depth0, "branchUrl", ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.ref), options)))
-    + "\">"
+  buffer += "\r\n<span class=\"action\">Deleted branch "
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.ref)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</a> at <a href=\""
+    + " at <a href=\""
     + escapeExpression((helper = helpers.repoUrl || (depth0 && depth0.repoUrl),options={hash:{},data:data},helper ? helper.call(depth0, ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options) : helperMissing.call(depth0, "repoUrl", ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options)))
     + "\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
@@ -109,11 +107,9 @@ function program1(depth0,data) {
 function program3(depth0,data) {
   
   var buffer = "", stack1, helper, options;
-  buffer += "\r\n<span class=\"action\">Deleted tag <a href=\""
-    + escapeExpression((helper = helpers.tagUrl || (depth0 && depth0.tagUrl),options={hash:{},data:data},helper ? helper.call(depth0, ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.ref), options) : helperMissing.call(depth0, "tagUrl", ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.ref), options)))
-    + "\">"
+  buffer += "\r\n<span class=\"action\">Deleted tag "
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.ref)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
-    + "</a> at <a href=\""
+    + " at <a href=\""
     + escapeExpression((helper = helpers.repoUrl || (depth0 && depth0.repoUrl),options={hash:{},data:data},helper ? helper.call(depth0, ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options) : helperMissing.call(depth0, "repoUrl", ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options)))
     + "\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
@@ -126,6 +122,92 @@ function program3(depth0,data) {
   buffer += "\r\n\r\n";
   stack1 = (helper = helpers.ifeq || (depth0 && depth0.ifeq),options={hash:{},inverse:self.noop,fn:self.program(3, program3, data),data:data},helper ? helper.call(depth0, ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.ref_type), "tag", options) : helperMissing.call(depth0, "ifeq", ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.ref_type), "tag", options));
   if(stack1 || stack1 === 0) { buffer += stack1; }
+  return buffer;
+  });
+Handlebars.partials['gh-DownloadEvent'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, helper, options, functionType="function", escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\r\n<span class=\"comment\">"
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.download)),stack1 == null || stack1 === false ? stack1 : stack1.description)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</span>\r\n";
+  return buffer;
+  }
+
+  buffer += "<span class=\"action\">Created new download <a href=\""
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.download)),stack1 == null || stack1 === false ? stack1 : stack1.html_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">"
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.download)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</a> for <a href=\""
+    + escapeExpression((helper = helpers.repoUrl || (depth0 && depth0.repoUrl),options={hash:{},data:data},helper ? helper.call(depth0, ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options) : helperMissing.call(depth0, "repoUrl", ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options)))
+    + "\">"
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</a></span>\r\n";
+  stack1 = helpers['if'].call(depth0, ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.download)),stack1 == null || stack1 === false ? stack1 : stack1.description), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  return buffer;
+  });
+Handlebars.partials['gh-FollowEvent'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, functionType="function", escapeExpression=this.escapeExpression, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\r\n  <a href=\""
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.target)),stack1 == null || stack1 === false ? stack1 : stack1.html_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">"
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.target)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + " ("
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.target)),stack1 == null || stack1 === false ? stack1 : stack1.login)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + ")</a>\r\n  ";
+  return buffer;
+  }
+
+function program3(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\r\n  <a href=\""
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.target)),stack1 == null || stack1 === false ? stack1 : stack1.html_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">"
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.target)),stack1 == null || stack1 === false ? stack1 : stack1.login)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</a>\r\n  ";
+  return buffer;
+  }
+
+function program5(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\r\n<span class=\"comment\">"
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.target)),stack1 == null || stack1 === false ? stack1 : stack1.bio)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</span>\r\n";
+  return buffer;
+  }
+
+  buffer += "<span class=\"action\">\r\n  Followed\r\n  ";
+  stack1 = helpers['if'].call(depth0, ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.target)),stack1 == null || stack1 === false ? stack1 : stack1.name), {hash:{},inverse:self.program(3, program3, data),fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += "\r\n</span>\r\n";
+  stack1 = helpers['if'].call(depth0, ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.target)),stack1 == null || stack1 === false ? stack1 : stack1.bio), {hash:{},inverse:self.noop,fn:self.program(5, program5, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  return buffer;
+  });
+Handlebars.partials['gh-ForkApplyEvent'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, functionType="function";
+
+
+  buffer += "<span class=\"action\">Applied fork commits to <a href=\""
+    + escapeExpression((helper = helpers.repoUrl || (depth0 && depth0.repoUrl),options={hash:{},data:data},helper ? helper.call(depth0, ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options) : helperMissing.call(depth0, "repoUrl", ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options)))
+    + "\">"
+    + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</a></span>";
   return buffer;
   });
 Handlebars.partials['gh-ForkEvent'] = template(function (Handlebars,depth0,helpers,partials,data) {
@@ -152,6 +234,31 @@ function program1(depth0,data) {
     + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.forkee)),stack1 == null || stack1 === false ? stack1 : stack1.full_name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
     + "</a></span>\r\n";
   stack1 = helpers['if'].call(depth0, ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.forkee)),stack1 == null || stack1 === false ? stack1 : stack1.description), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  return buffer;
+  });
+Handlebars.partials['gh-GistEvent'] = template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, helper, options, functionType="function", escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing, self=this;
+
+function program1(depth0,data) {
+  
+  var buffer = "", stack1;
+  buffer += "\r\n<span class=\"comment\">\r\n"
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.gist)),stack1 == null || stack1 === false ? stack1 : stack1.description)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\r\n</span>\r\n";
+  return buffer;
+  }
+
+  buffer += "<span class=\"action\">"
+    + escapeExpression((helper = helpers.capitalise || (depth0 && depth0.capitalise),options={hash:{},data:data},helper ? helper.call(depth0, ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.action), options) : helperMissing.call(depth0, "capitalise", ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.action), options)))
+    + "d gist <a href=\""
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.gist)),stack1 == null || stack1 === false ? stack1 : stack1.html_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">"
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.gist)),stack1 == null || stack1 === false ? stack1 : stack1.id)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</a></span>\r\n";
+  stack1 = helpers['if'].call(depth0, ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.gist)),stack1 == null || stack1 === false ? stack1 : stack1.description), {hash:{},inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
   return buffer;
   });
@@ -213,10 +320,14 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 Handlebars.partials['gh-MemberEvent'] = template(function (Handlebars,depth0,helpers,partials,data) {
   this.compilerInfo = [4,'>= 1.0.0'];
 helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, helper, options, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, functionType="function";
+  var buffer = "", stack1, helper, options, functionType="function", escapeExpression=this.escapeExpression, helperMissing=helpers.helperMissing;
 
 
-  buffer += "<span class=\"action\">Became a contributor to <a href=\""
+  buffer += "<span class=\"action\">Made <a href=\""
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.member)),stack1 == null || stack1 === false ? stack1 : stack1.html_url)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "\">"
+    + escapeExpression(((stack1 = ((stack1 = ((stack1 = (depth0 && depth0.payload)),stack1 == null || stack1 === false ? stack1 : stack1.member)),stack1 == null || stack1 === false ? stack1 : stack1.login)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
+    + "</a>\r\na contributor to <a href=\""
     + escapeExpression((helper = helpers.repoUrl || (depth0 && depth0.repoUrl),options={hash:{},data:data},helper ? helper.call(depth0, ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options) : helperMissing.call(depth0, "repoUrl", ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name), options)))
     + "\">"
     + escapeExpression(((stack1 = ((stack1 = (depth0 && depth0.repo)),stack1 == null || stack1 === false ? stack1 : stack1.name)),typeof stack1 === functionType ? stack1.apply(depth0) : stack1))
